479db1485d80717b5b7f62424497763879d4fd3e
locklink <yiyangit@qq.com>
2026-04-03 00:46:01 +0800

    Site updated: 2026-04-03 00:46:00

--- js/main.js
+++ js/main.js
@@ -10,6 +10,153 @@ if (!!$.prototype.justifiedGallery) {
   $(".article-gallery").justifiedGallery(options);
 }
 
+function typesetMathInElement(element) {
+  if (!element) {
+    return;
+  }
+
+  if (element.getAttribute("data-mathjax-rendered") === "true") {
+    return;
+  }
+
+  if (window.MathJax && window.MathJax.Hub && typeof window.MathJax.Hub.Queue === "function") {
+    window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub, element]);
+    window.MathJax.Hub.Queue(function() {
+      element.setAttribute("data-mathjax-rendered", "true");
+    });
+    return;
+  }
+
+  if (window.MathJax && typeof window.MathJax.typesetPromise === "function") {
+    window.MathJax.typesetPromise([element]).then(function() {
+      element.setAttribute("data-mathjax-rendered", "true");
+    });
+  }
+}
+
+function ensureMathInElement(selector, retriesLeft) {
+  var element = document.querySelector(selector);
+
+  if (!element) {
+    return;
+  }
+
+  if ((window.MathJax && window.MathJax.Hub) || (window.MathJax && typeof window.MathJax.typesetPromise === "function")) {
+    typesetMathInElement(element);
+    return;
+  }
+
+  if (retriesLeft > 0) {
+    window.setTimeout(function() {
+      ensureMathInElement(selector, retriesLeft - 1);
+    }, 300);
+  }
+}
+
+function getHeadingContentHtml(heading) {
+  var clone = heading.cloneNode(true);
+  var anchor = clone.querySelector(".markdownIt-Anchor");
+
+  if (anchor) {
+    anchor.remove();
+  }
+
+  return clone.innerHTML.trim();
+}
+
+function buildTocHtml() {
+  var headings = Array.from(document.querySelectorAll(".post .content h1[id], .post .content h2[id], .post .content h3[id], .post .content h4[id], .post .content h5[id], .post .content h6[id]"));
+
+  if (!headings.length) {
+    return "";
+  }
+
+  var minLevel = headings.reduce(function(result, heading) {
+    return Math.min(result, Number(heading.tagName.slice(1)));
+  }, 6);
+  var root = document.createElement("ol");
+  var listStack = [root];
+  var itemStack = [];
+  var currentLevel = minLevel;
+  var counters = [0, 0, 0, 0, 0, 0];
+
+  root.className = "toc";
+
+  headings.forEach(function(heading) {
+    var level = Number(heading.tagName.slice(1));
+    var tocLevel = level - minLevel;
+    var parentList;
+    var item;
+    var link;
+    var number;
+    var numberSpan;
+    var textSpan;
+
+    while (currentLevel < level) {
+      if (!itemStack.length) {
+        break;
+      }
+
+      parentList = document.createElement("ol");
+      parentList.className = "toc-child";
+      itemStack[itemStack.length - 1].appendChild(parentList);
+      listStack.push(parentList);
+      currentLevel += 1;
+    }
+
+    while (currentLevel > level) {
+      listStack.pop();
+      itemStack.pop();
+      currentLevel -= 1;
+    }
+
+    counters[tocLevel] += 1;
+    for (var i = tocLevel + 1; i < counters.length; i += 1) {
+      counters[i] = 0;
+    }
+
+    item = document.createElement("li");
+    item.className = "toc-item toc-level-" + level;
+
+    link = document.createElement("a");
+    link.className = "toc-link";
+    link.setAttribute("href", "#" + heading.id);
+
+    number = counters.slice(0, tocLevel + 1).join(".") + ".";
+    numberSpan = document.createElement("span");
+    numberSpan.className = "toc-number";
+    numberSpan.textContent = number;
+
+    textSpan = document.createElement("span");
+    textSpan.className = "toc-text";
+    textSpan.innerHTML = getHeadingContentHtml(heading);
+
+    link.appendChild(numberSpan);
+    link.appendChild(document.createTextNode(" "));
+    link.appendChild(textSpan);
+    item.appendChild(link);
+    listStack[listStack.length - 1].appendChild(item);
+    itemStack[level - minLevel] = item;
+    itemStack.length = level - minLevel + 1;
+  });
+
+  return root.outerHTML;
+}
+
+function syncTocFromHeadings() {
+  var tocHtml = buildTocHtml();
+  var toc = document.querySelector("#toc");
+  var tocFooter = document.querySelector("#toc-footer");
+
+  if (toc && tocHtml) {
+    toc.innerHTML = tocHtml;
+  }
+
+  if (tocFooter && tocHtml) {
+    tocFooter.innerHTML = tocHtml;
+  }
+}
+
 $(document).ready(function() {
 
   /**
@@ -25,6 +172,10 @@ $(document).ready(function() {
    * for Desktop, tablet and mobile.
    */
   if ($(".post").length) {
+    syncTocFromHeadings();
+    ensureMathInElement("#toc", 20);
+    ensureMathInElement("#toc-footer", 20);
+
     var menu = $("#menu");
     var nav = $("#menu > #nav");
     var menuIcon = $("#menu-icon, #menu-icon-tablet");
@@ -49,6 +200,10 @@ $(document).ready(function() {
         menuIcon.removeClass("active");
       }
       return false;
+    });
+
+    $("#actions-footer > #toc").click(function() {
+      ensureMathInElement("#toc-footer", 20);
     });
 
     /**
